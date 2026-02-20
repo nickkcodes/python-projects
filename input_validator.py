8bafd454a8df25089818376521b30532803b7e86
nickkcodes <mayumicalamno5@gmail.com>
2026-02-19 20:16:57 -0800

    Create input_validator.py

---
+++ input_validator.py
@@ -0,0 +1,9 @@
+while True:
+    name = input('What is your name? ')
+    if len(name) < 3:
+        print('Name must be at least 3 characters')
+    elif len(name) > 50:
+        print('Name can be a maximum of 50 characters')
+    else:
+        print('Looks good!')
+        break
