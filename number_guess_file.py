707e60725f65e4c2479afa29e00927035ea00c77
nickkcodes <mayumicalamno5@gmail.com>
2026-02-28 22:45:50 -0800

    number_guess_file.py
    "Number guessing game with input validation and error handling."

--- number_guess_file.py
+++ number_guess_file.py
@@ -1,11 +1,20 @@
-secret_number = 6
+secret_number = 67
 guess_count = 0
 guess_limit = 3
+
 while guess_count < guess_limit:
-    guess = int(input('Guess a number: '))
-    guess_count += 1
+    try:
+        guess = int(input('Guess the number: '))
+        guess_count += 1
+    except ValueError:
+        print('Number must be an integer.')
+        continue
+
     if guess == secret_number:
-        print('You won!')
+        print('Congratulations! You guessed the number!')
         break
+
+    print(f'You have {guess_limit - guess_count} tries left')
+
 else:
-    print('You lost')
+    print('You lost.')
